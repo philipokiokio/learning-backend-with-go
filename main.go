01babd01a8c5f2068581ed052e4f180c39a104e9
Philip <philipokiokio@gmail.com>
2023-05-03 07:44:59 +0100

    Fresh config for dyanamic reload

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my awesome Site </h1>")
+	fmt.Fprintf(w, "<h1>Welcome to my awesome Site! </h1>")
 }
 
 func main() {
