8ed7fda9147101141111a7f771ca4aea0acf8727
Philip <philipokiokio@gmail.com>
2023-05-04 06:14:55 +0100

    contact page, page not found and setting status code based on response

--- main.go
+++ main.go
@@ -6,12 +6,28 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my awesome Site! </h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/"{
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "<h1>Welcome to my awesome Site! </h1>")
+
+	} else if r.URL.Path == "/contact/"{
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "To get in touch, please send an email to <a href=\"mailto:go@gotunnel.com\">go@gotunnel.com</a>")
+
+	}else{
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w,"<h1>404 Page Not Found</h1>")
+	}
+
+
+
 }
 
 func main() {
 
 	http.HandleFunc("/", handlerFunc)
+
 	fmt.Println("Server started at http://localhost:4590!")
 
 	http.ListenAndServe(":4590", nil)
